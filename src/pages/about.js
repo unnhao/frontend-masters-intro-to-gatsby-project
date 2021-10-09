3b111d948f006ac50a07a558e26466d74ab83081
Hao <unnhao@gmail.com>
2021-10-10 00:36:15 +0800

    create pages

---
+++ src/pages/about.js
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+export default function AboutPage() {
+  return <main>
+      <h1>About This Site</h1>
+  </main>;
+}
---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+export default function IndexPage() {
+  return <main>
+      <h1>Hello Frontend Master!</h1>
+  </main>;
+}
